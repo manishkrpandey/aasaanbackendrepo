cee41577c326e077248638ca08fb85653999086b
MANISH KUMAR PANDEY <mkpandey015@gmail.com>
2020-08-19 06:00:35 +0530

    added in index file

--- index.js
+++ index.js
@@ -61,3 +61,147 @@ app.use(symbols.API_CUSTOMER,customerController);
 app.get(symbols.GET_API_ALL, function (req, res) {
   common.send(res,symbols.JSON_COMMANDS);
 });
+
+app.get(symbols.GET_RESTAURENT_DATA, function (req, res){
+  let data =  {
+      restaurantName:'Maharaja Hotel',
+      status:'open',
+      location:{
+        name:'Patna',
+        longitude:'100',
+        latitude:'105'
+      },
+      todayRevenue:{
+        totalRevenue:10000,
+        totalOrder:100
+      },
+      monthRevenue:{
+        totalRevenue:10000,
+        totalOrder:100
+      }
+    }
+  common.sendResponse(data, symbols.CONSTANT_RESPONSE_SUCCESS, 'fetched');
+});
+
+app.get(symbols.GET_MENU_DATA, function (req, res){
+let data =  {
+      restarentName:'',
+      category:[
+        {
+          name:'Deserts',
+          menuitems:[
+              {
+                type:'1',  
+                name:'Rasgulla',
+                status:'available',
+                imageUrl:'https://i1.wp.com/www.crispyfoodidea.com/wp-content/uploads/2020/04/RASGULLA-CRISPYFOODIDEA.jpg?w=900&ssl=1',
+                variant:[{
+                  name:'piece',
+                  price:10
+                }]},
+                {
+                  type:'1',
+                name:'Halwa',
+                status:'available',
+                imageUrl:'https://i1.wp.com/www.crispyfoodidea.com/wp-content/uploads/2020/04/RASGULLA-CRISPYFOODIDEA.jpg?w=900&ssl=1',
+                variant:[{
+                  name:'100 gm',
+                  price:100
+                },
+                {
+                  name:'200 gm',
+                  price:200
+                }
+                ]},
+                {
+                  type:'1',
+                name:'Galebi',
+                status:'available',
+                imageUrl:'https://i1.wp.com/www.crispyfoodidea.com/wp-content/uploads/2020/04/RASGULLA-CRISPYFOODIDEA.jpg?w=900&ssl=1',
+                variant:[{
+                  name:'100gm',
+                  price:100
+                },
+                {
+                  name:'1000gm',
+                  price:1000
+                }
+                ]},
+                {
+                  type:'1',
+                name:'Rasgulla',
+                status:'available',
+                imageUrl:'https://i1.wp.com/www.crispyfoodidea.com/wp-content/uploads/2020/04/RASGULLA-CRISPYFOODIDEA.jpg?w=900&ssl=1',
+                variant:[{
+                  name:'piece',
+                  price:10
+                }]}
+                
+            ]
+        },
+            {
+          name:'Dinner/lunch',
+          menuitems:[{
+                type:'1',  
+                name:'Paneer',
+                status:'available',
+                imageUrl:'https://i1.wp.com/www.crispyfoodidea.com/wp-content/uploads/2020/04/RASGULLA-CRISPYFOODIDEA.jpg?w=900&ssl=1',
+                variant:[{
+                  name:'1/4',
+                  price:100
+                },
+                {
+                  name:'1/2',
+                  price:20
+                },
+                {
+                  name:'Full',
+                  price:40
+                }
+                ]},
+                {
+                  type:'2',
+                name:'Chicken',
+                status:'available',
+                imageUrl:'https://i1.wp.com/www.crispyfoodidea.com/wp-content/uploads/2020/04/RASGULLA-CRISPYFOODIDEA.jpg?w=900&ssl=1',
+                variant:[{
+                  name:'100 gm',
+                  price:100
+                },
+                {
+                  name:'200 gm',
+                  price:200
+                }
+                ]},
+                {
+                  type:'1',
+                name:'Rajma',
+                status:'available',
+                imageUrl:'https://i1.wp.com/www.crispyfoodidea.com/wp-content/uploads/2020/04/RASGULLA-CRISPYFOODIDEA.jpg?w=900&ssl=1',
+                variant:[{
+                  name:'100gm',
+                  price:100
+                },
+                {
+                  name:'1000gm',
+                  price:1000
+                }
+                ]},
+                {
+                  type:'2',
+                name:'Biryani',
+                status:'available',
+                imageUrl:'https://i1.wp.com/www.crispyfoodidea.com/wp-content/uploads/2020/04/RASGULLA-CRISPYFOODIDEA.jpg?w=900&ssl=1',
+                variant:[{
+                  name:'piece',
+                  price:10
+                }]}
+                
+            ]
+        },
+        
+        ]
+    }
+    
+  common.sendResponse(data, symbols.CONSTANT_RESPONSE_SUCCESS, 'fetched');
+});
